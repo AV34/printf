c60f9de4524b14480552ab4f64e58f9e2ebf4818
Minniemaey <ayodedeyi@gmail.com>
2023-08-25 03:43:53 -0700

    update buffer print

---
+++ print_pointer.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * print_pointer - * variable printing
+ * @types: arg list
+ * @buffer: Buffer []
+ * @flags:  checks active flags
+ * @width: width
+ * @precision: Precision
+ * @size: Size
+ * Return: num of chars to be printed.
+ */
+int print_pointer(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+	char extra_c = 0, padding = ' ';
+	int index = BUFF_SIZE - 2, length = 2, padding_start = 1;
+	unsigned long num_addrs;
+	char map_to[] = "0123456789abcdef";
+	void *addrs = va_arg(types, void *);
+
+	UNUSED(width);
+	UNUSED(size);
+
+	if (addrs == NULL)
+		return (write(1, "(nil)", 5));
+
+	buffer[BUFF_SIZE - 1] = '\0';
+	UNUSED(precision);
+
+	num_addrs = (unsigned long)addrs;
+
+	while (num_addrs > 0)
+	{
+		buffer[index--] = map_to[num_addrs % 16];
+		num_addrs /= 16;
+		length++;
+	}
+
+	if ((flags & F_ZERO) && !(flags & F_MINUS))
+		padding = '0';
+	if (flags & F_PLUS)
+		extra_c = '+', length++;
+	else if (flags & F_SPACE)
+		extra_c = ' ', length++;
+
+	index++;
+	return (write_pointer(buffer, index, length,
+		width, flags, padding, extra_c, padding_start));
+}
