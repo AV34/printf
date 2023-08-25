d10123893bae6e459d70cf23596bb76ada0dae36
Minniemaey <ayodedeyi@gmail.com>
2023-08-25 02:56:10 -0700

    print function update

---
+++ print_octal.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * print_octal - function to print unsigned octal number
+ * @types: argument list
+ * @buffer: Buffer [] to handle printing
+ * @flags: cals active flags
+ * @width: width
+ * @precision: Precision
+ * @size: Size
+ * Return: chars to be printed.
+ */
+int print_octal(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
+{
+
+	int a = BUFF_SIZE - 2;
+	unsigned long int num = va_arg(types, unsigned long int);
+	unsigned long int init_num = num;
+
+	UNUSED(width);
+
+	num = convert_size_unsgnd(num, size);
+
+	if (num == 0)
+		buffer[a--] = '0';
+
+	buffer[BUFF_SIZE - 1] = '\0';
+
+	while (num > 0)
+	{
+		buffer[a--] = (num % 8) + '0';
+		num /= 8;
+	}
+
+	if (flags & F_HASH && init_num != 0)
+		buffer[a--] = '0';
+
+	a++;
+
+	return (write_unsgnd(0, a, buffer, flags, width, precision, size));
+}
