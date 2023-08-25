9b7ab5a50b516202c081948f63c54ee25b6e26e4
Minniemaey <ayodedeyi@gmail.com>
2023-08-25 09:03:52 -0700

    printf update

--- _printf.c
+++ _printf.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+void print_buffer(char buffer[], int *buff_index);
+
 /**
 * _printf - function that prints output to format
 * @format: character format
@@ -7,44 +9,44 @@
 */
 int _printf(const char *format, ...)
 {
-int i, print_char = 0, print_chars = 0;
-int flags, precision, size, width, buff_index = 0;
-va_list args_list;
-char buffer[BUFF_SIZE];
+	int i, print_char = 0, print_chars = 0;
+	int flags, precision, size, width, buff_index = 0;
+	va_list args_list;
+	char buffer[BUFF_SIZE];
+	
+	if (format == NULL)
+		return (-1);
+	
+	va_start(args_list, format);
 
-if (format == NULL)
-return (-1);
-
-va_start(args_list, format);
-
-for (i = 0; format && format[i] != '\0'; i++)
-{
-if (format[i] != '%')
-{
-buffer[buff_index++] = format[i];
-if (buff_index == BUFF_SIZE)
-print_buffer(buffer, &buff_index);
-print_chars++;
-}
-else
-{
-print_buffer(buffer, &buff_index);
-flags = get_flags(format, &i);
-width = get_width(format, &i, args_list);
-precision = get_precision(format, &i, args_list);
-size = get_size(format, &i);
-++i;
-print_char = getprint_handler(format, &i, args_list,
-buffer, flags, width, precision, size);
-if (print_char == -1)
-return (-1);
-print_chars += print_char;
-}
-}
-print_buffer(buffer, &buff_index);
-
-va_end(args_list);
-return (print_chars);
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			buffer[buff_index++] = format[i];
+			if (buff_index == BUFF_SIZE)
+				print_buffer(buffer, &buff_index);
+			/*write(1, &format[i], 1);*/
+			print_chars++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_index);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, args_list);
+			precision = get_precision(format, &i, args_list);
+			size = get_size(format, &i);
+			++i;
+			print_char = getprint_handler(format, &i, args_list,
+					buffer, flags, width, precision, size);
+			if (print_char == -1)
+				return (-1);
+			print_chars += print_char;
+		}
+	}
+	print_buffer(buffer, &buff_index);
+	va_end(args_list);
+	return (print_chars);
 }
 
 /**
@@ -54,8 +56,7 @@ return (print_chars);
 */
 void print_buffer(char buffer[], int *buff_index)
 {
-if (*buff_index > 0)
-write(1, &buffer[0], *buff_index);
-
-*buff_index = 0;
+	if (*buff_index > 0)
+		write(1, &buffer[0], *buff_index);
+	*buff_index = 0;
 }
